4dbc1704af6a45dcc94bfb08fa2c91b3bb028fe3
rafcio6765-afk <rafcio6765@gmail.com>
2026-03-20 20:11:50 +0100

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-20T19:11:18.307Z
-Commit: Add multi-language support
+Timestamp: 2026-03-20T19:11:49.179Z
+Commit: Patch security vulnerability in authentication
